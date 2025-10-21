67bed63412a14faccb0ca23db44e4dd1f622d94f
fabiokleis <fabiohkrc@gmail.com>
2025-10-21 00:09:46 -0300

    basic put symbol on cell position

--- tictactoe.hs
+++ tictactoe.hs
@@ -2,7 +2,8 @@ module Main where
 
 type Pos = (Int, Int)
 type Cells = [[Symbol]]
-
+type Width = Int
+type Height = Int
 
 data Symbol = Emp | Sep | Op | Xp
 
@@ -12,7 +13,9 @@ instance Show Symbol where
   show Op  = "O"
   show Xp  = "X"
 
-data Board = Board Cells Int Int deriving Show
+data Pl
+
+data Board = Board Cells Width Height deriving Show
 
 newBoard :: Int -> Int -> Board
 newBoard w h =
@@ -24,16 +27,38 @@ pprintBoard (x:xs) w = do
   let t = tail x
   let h = head x
   let mapped = concat [[h], (concat $ map (\s -> [Sep] ++ [s]) t)]
-  putStrLn $ concat $ replicate w "__"
-  putStrLn (concat $ map (\s -> show s) mapped)
   pprintBoard xs w
+  putStrLn (concat $ map (\s -> show s) mapped)
+  putStrLn $ concat $ replicate w "__"
 
 changeBoard :: Board -> Symbol -> Pos -> Board
-changeBoard = undefined
-  
+changeBoard (Board b h w) symbol pos = do
+  let (fh, sh) = splitAt (fst pos) b
+  let row = last fh
+  let rest = take ((fst pos) - 1) fh
+  let (rfh, rsh) = splitAt (snd pos) row
+  let rrest = take ((snd pos) - 1) row
+  let nrow = rest ++ [rrest ++ [symbol] ++ rsh]
+  let cells = nrow ++ sh
+  Board cells h w
+
+gameLoop :: Board -> IO ()
+gameLoop b = do
+  let (Board c w _) = b
+  putStrLn "Board"
+  pprintBoard c w
+
+  putStrLn "press enter to continue, q or quit to stop: "
+  opt <- getLine
+  case opt of
+    "q" -> putStrLn "exit..."
+    "quit" -> putStrLn "exiting ..."
+    "" -> gameLoop b
+    "help" -> putStrLn "tictactoe options: (q)uit, help or enter to continue"
+    _ -> putStrLn "exit..."
+
 
 main :: IO ()
 main = do
-  let (Board c w _) = newBoard 3 3
-  putStrLn "Board"
-  pprintBoard c w
+  let board = newBoard 3 3
+  gameLoop board
